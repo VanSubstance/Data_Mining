372b3077dc06ccddab6e555b795556bca4342418
VanSubstance <50601968+VanSubstance@users.noreply.github.com>
2020-05-26 00:35:51 +0900

    Update Naive_Bayes.py

--- Assignment_02/Naive_Bayes.py
+++ Assignment_02/Naive_Bayes.py
@@ -73,12 +73,18 @@ for i in range(trainX.shape[1]):
 
 binaryX = (trainX > trainX.mean(axis = 0)) ** 1
 
+from sklearn.naive_bayes import BernoulliNB
+model_bern = BernoulliNB(alpha = 1).fit(binaryX, trainY)
 
 # (3) Based on the calculated p_ij, calculate probability of class g for each test sample (testX) and calculate accuracy for testX with varying cutoff (To binarize testX, use the mean of trainX). Prior probabilities of classes are proportional to ratios of classes in training set. cutoff ∈{0.1,0.15,0.2,0.25,…,0.95}. Draw a line plot (x=cutoff, y=accuracy).
 
 # Probability of class g
+prob_2= BNB(binaryX, trainY)
 n, p = testX.shape
-binaryX = binaryX = (testX > testX.mean(axis = 0)) ** 1
+binaryX = binaryX = (testX > trainX.mean(axis = 0)) ** 1
+
+ptr = list(model_bern.predict_proba(binaryX)[:, 0])
+
 avg = []
 for i in range(p):
     avg.append(np.mean(trainX[:, i]))
@@ -86,34 +92,42 @@ for i in range(p):
 print("BNB:")
 print(BNB(binaryX, testY))
 
-prob= BNB(binaryX, testY)[0]
+
 prior_g = list(testY).count('g') / n # Prior P(y = 'g')
 
-# likelihood -> Posterior probability
+# Posterior probability
 
 prob_g = [] # array of Posterior probability for each test sample: P(y = 'g' | x = 1) (testX)
 
-for i in range(n): # likelihood: P(x = 1 | y = 'g')
+for i in range(n): # likelihood: P(x = 1 | y = 'g'), P(x = 1 | y = 'h')
     temp = [1, 1]
     for j in range(p):
         if binaryX[i, j] == 1:
-            temp[0] *= prob[j]
-            temp[1] *= (1 - prob[j])
+            temp[0] *= prob_2[0][j]
+            temp[1] *= prob_2[1][j]
         else:
-            temp[1] *= prob[j]
-            temp[0] *= (1 - prob[j])
-            
-        # P(y = 'g' | x = 1) ∝ P(x = 1 | y = 'g') * P(y = 'g')
-        temp[0] *= prior_g
-        temp[1] *= prior_g
-        
-    # P(y = 'g' | x = 1) ∝ P(x = 1 | y = 'g') * P(y = 'g') / P(x)
+            temp[0] *= (1 - prob_2[0][j])
+            temp[1] *= (1 - prob_2[1][j])
+    # P(x = 1 | y = 'g') * P(y = 'g'), P(x = 1 | y = 'h') * P(y = 'h')
+    temp[0] *= prior_g
+    temp[1] *= (1 - prior_g)
+    
     prob_g.append(list(temp / sum(temp)))
 
 prob_g = np.array(prob_g)
 prob_g = prob_g[:, 0]
 
 
+
+print("P(x = 1 | y = 'g') based on BNB:")
+print(prob_2, "\n")
+
+print("P(x = 1 | y = 'g') based on sklearn:")
+print(np.exp(model_bern.feature_log_prob_)[0])
+
+print("BNB P(y = 'g'): ", prob_g)
+print("sklearn P(y = 'g'): ", ptr)
+
 ### Method for calculating accuracy by comparing two array
 
 def score_calc(A, B):
@@ -123,73 +137,62 @@ def score_calc(A, B):
             count += 1
     return count / len(B)
 
-### Calculating accuracy with varying cutoff
+### Calculating accuracy with varying cutoff: BNB
 
-cutoff = np.arange(0.1, 1, 0.05)
-set_Y = []
+cutoff = np.arange(0.1, 1, 0.05) # array of cutoff
+set_Y = [] # array of estimated y based on prob_g
 for i in cutoff:
     cutoff_Y = ['g' if j >= i else 'h' for j in prob_g]
     set_Y.append(cutoff_Y)
 
-acc_Y = []
+acc_Y = [] # array of accuracy based on set_y and testY
 for i in range(len(set_Y)):
     acc_Y.append(score_calc(set_Y[i], testY))
-
-
-print("Corresponding ratio between cutoff .1 and .95:\n", score_calc(set_Y[0], set_Y[17]))
-
-print("sklearn:")
-
-from sklearn.naive_bayes import BernoulliNB
-model_bern = BernoulliNB(alpha = 1).fit(binaryX, testY)
-
-print(np.exp(model_bern.feature_log_prob_))
-
-ptr = list(model_bern.predict_proba(binaryX)[:, 0])
-### Method for calculating accuracy by comparing two array
-
-def score_calc(A, B):
-    count = 0
-    for i in range(len(B)):
-        if A[i] == B[i]:
-            count += 1
-    return count / len(B)
+    
 
 ### Calculating accuracy with varying cutoff
 
-cutoff = np.arange(0.1, 1, 0.05)
-set_Y = []
+set_Y_sklearn = []
 for i in cutoff:
     cutoff_Y = ['g' if j >= i else 'h' for j in ptr]
-    set_Y.append(cutoff_Y)
+    set_Y_sklearn.append(cutoff_Y)
 
-acc_Y = []
+### set_Y_sklearn & testY
+
+acc_Y_sklearn_calc = []
 for i in range(len(set_Y)):
-    acc_Y.append(score_calc(set_Y[i], testY))
-
-
-print("Corresponding ratio between cutoff .1 and .95:\n", score_calc(set_Y[0], set_Y[17]))
-
-acc_Y_score = []
+    acc_Y_sklearn_calc.append(score_calc(set_Y_sklearn[i], testY))
+    
+acc_Y_sklearn = []
 for i in set_Y:
     accuracy_Y = model_bern.score(binaryX, i)
-    acc_Y_score.append(accuracy_Y)
+    acc_Y_sklearn.append(accuracy_Y)
     
-plt.figure(figsize = (10, 5))
-plt.plot(cutoff, acc_Y, color= "blue", label = "BNB")
-plt.plot(cutoff, acc_Y_score, color = "red", label = "sklearn")
+plt.figure(figsize = (20, 15))
+plt.plot(cutoff, acc_Y_sklearn_calc, color = "green", label = "Model_calculated")
+plt.plot(cutoff, acc_Y_sklearn, color = "red", label = "Model_score")
+plt.plot(cutoff, acc_Y, color = "blue", label = "BNB_calculated")
 plt.title("Cutoff ~ Accuracy")
 plt.xlabel("cutoff")
 plt.ylabel("Accuracy")
 plt.xticks(cutoff)
+plt.legend(loc = 0, prop = {"size" : 20})
+plt.show
+
+plt.figure(figsize = (20, 15))
+plt.plot(prob_g, 'x', color = "blue", label = "BNB")
+plt.plot(list(model_bern.predict_proba(binaryX)[:, 0]), '+', color = "red", label = "Model")
+plt.legend(loc = 0, prop = {"size": 20})
 plt.show()
+
+"""
+ANSWER
+"""
 
 plt.figure(figsize = (10, 5))
-plt.plot(prob_g, '+', color = "blue", label = "BNB")
-plt.plot(ptr, 'x', color = "red", label = "sklearn")
-plt.title("Scatter of P(y = 'g')")
-plt.xlabel("number")
-plt.ylabel("probability")
-plt.show()
-
-
+plt.plot(cutoff, acc_Y)
+plt.title("Cutoff ~ Accuracy")
+plt.xlabel("cutoff")
+plt.ylabel("Accuracy")
+plt.xticks(cutoff)
+plt.show
